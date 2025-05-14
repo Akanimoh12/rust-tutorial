6ea1a5d615e029009bf857a7549d536153a057ca
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-05-15 00:09:31 +0100

    Tested some arrays and tuples

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    // Number Data Type
     let x = 6;
     println!("Hello world and {}", x);
 
@@ -6,6 +7,22 @@ fn main() {
     let b = 7;
     println!("\n{}", a + b );
 
+    // Constant Data Type must be written in Capital from my understanding
     const PI: f64 = 3.123;
     println!("\n{}", PI );
+
+    // Boolean Data Type for True or False
+    let is_name = true;
+    print!("\n{}", is_name);
+
+    // Character uses one Quote
+    let letter = "A";
+    print!("\n\n{}", letter);
+
+    
+    let array   = ["Hello", "Hi", "Great"];
+    print!("\n{:#?}", array[0]);
+
+    let mixture = (PI, is_name, letter, array);
+    print!("\n\n{:#?}", mixture.3[2]);
 }
