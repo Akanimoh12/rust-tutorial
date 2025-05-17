ede730eda211aca9b4931af4bd409a424597236b
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-05-17 19:23:07 +0100

    Run some struct data types

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#(derive)
+#[derive(Debug)]
 struct Student {
         name: String,
         reg_number: u32,
@@ -19,53 +19,55 @@ fn main() {
     email:"akanimoh@gmail.com".to_string(),
     };
 
+    print!("{:#?}", student);
 
 
-    // Number Data Type
-    let x = 6;
-    println!("Hello world and {}", x);
 
-    let a = 6;
-    let b = 7;
-    println!("\n{}", a + b );
+    // // Number Data Type
+    // let x = 6;
+    // println!("Hello world and {}", x);
 
-    // Constant Data Type must be written in Capital from my understanding
-    const PI: f64 = 3.123;
-    println!("\n{}", PI );
+    // let a = 6;
+    // let b = 7;
+    // println!("\n{}", a + b );
 
-    // Boolean Data Type for True or False
-    let is_name = true;
-    print!("\n{}", is_name);
+    // // Constant Data Type must be written in Capital from my understanding
+    // const PI: f64 = 3.123;
+    // println!("\n{}", PI );
 
-    // Character uses one Quote
-    let letter = "A";
-    print!("\n\n{}", letter);
+    // // Boolean Data Type for True or False
+    // let is_name = true;
+    // print!("\n{}", is_name);
 
-    // array    
-    let array   = ["Hello", "Hi", "Great"];
-    print!("\n{:#?}", array[0]);
+    // // Character uses one Quote
+    // let letter = "A";
+    // print!("\n\n{}", letter);
 
-    // Tuples: i used tuples to return all my other datas
-    let mixture = (PI, is_name, letter, array);
-    print!("\n\n{:#?}", mixture.3[2]);
+    // // array    
+    // let array   = ["Hello", "Hi", "Great"];
+    // print!("\n{:#?}", array[0]);
 
-    // Control Flows
-    if array.len() == 3 {
-        print!("\n Is successful")
-    }else {
-        print!("\n Not Successful")
-    }
+    // // Tuples: i used tuples to return all my other datas
+    // let mixture = (PI, is_name, letter, array);
+    // print!("\n\n{:#?}", mixture.3[2]);
 
-    let name = "Christiana".to_string();
-    print!("\nv1: {}", name);
+    // // Control Flows
+    // if array.len() == 3 {
+    //     print!("\n Is successful")
+    // }else {
+    //     print!("\n Not Successful")
+    // }
 
-    let mut new_name = name.as_str();
-    print!("\n\nv2: {}\n", new_name);
+    // let name = "Christiana".to_string();
+    // print!("\nv1: {}", name);
 
-    new_name = "Tali";
-    print!("\n New v2: {} \n\n", new_name);
+    // let mut new_name = name.as_str();
+    // print!("\n\nv2: {}\n", new_name);
 
-    // print!("\n name {} \n\n", name);
+    // new_name = "Tali";
+    // print!("\n New v2: {} \n\n", new_name);
+
+    print!("\n\n");
 
     
 
