25740167b7c3732ef6032d340ebdca76e85947e8
akanimoh12 <akanimohjohnson2003@gmail.com>
2025-05-26 13:42:37 +0100

    Updated

--- src/libs.rs
+++ src/libs.rs
@@ -14,8 +14,8 @@
         #[test]
         fn test_add() {
 
-            let a: u64 = 21;
-            let b: u64 = 21;
+            let a: u64 = 20;
+            let b: u64 = 20;
             let result = add(a, b);
             println!("{}", result);
             assert_eq!(result, 42);
